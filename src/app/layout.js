40ba9dee2d63c6f85e5253b8f64e35f53d14f686
Hamza-Aigumind <hamza.khalid@aigumind.com>
2025-08-06 12:27:02 +0500

    pro

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,6 @@
-import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
 
 export const metadata = {
   title: "BigCommerce Migration Tool",
@@ -16,20 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} font-sans antialiased`}>
+      <body>
+        {/* <ToastContainer /> */}
         {children}
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-        />
       </body>
     </html>
   );
